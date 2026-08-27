396e635c4ad4f2c7532d57e4cb80d33d4c9c937d
Juimun <2002kofanov@gmail.com>
2026-08-27 16:57:33 +0300

    feat: bootstrap application and GraphQL server
    - configure GraphQLModule with Apollo driver and auto schema
    - enable Apollo Sandbox landing page
    - import PrismaModule and ProfileModule into AppModule
    - create application entry point

---
+++ src/app.module.ts
@@ -0,0 +1,23 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+import { PrismaModule } from './prisma/prisma.module';
+import { ProfileModule } from './profile/profile.module';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      csrfPrevention: false,
+    }),
+    PrismaModule,
+    ProfileModule,
+  ],
+})
+export class AppModule {}
